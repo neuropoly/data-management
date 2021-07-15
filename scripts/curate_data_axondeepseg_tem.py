8ba5a5cc9fee00a9f17295d40d82c228834b485a
Alexandru Foias <alexfoias@users.noreply.github.com>
2021-07-15 10:25:18 -0400

    Update scripts/curate_data_axondeepseg_tem.py
    Co-authored-by: Nick <nick.guenther@polymtl.ca>

--- scripts/curate_data_axondeepseg_tem.py
+++ scripts/curate_data_axondeepseg_tem.py
@@ -45,7 +45,7 @@ def create_json_sidecar(path_folder_sub_id_bids, item_out):
 def main(root_data, output_data):
 
     # Remove macOS .DS_Store
-    os.system("find " + root_data + " -name '.DS_Store' -type f -delete")
+    subprocess.run(["find", root_data, "-name", ".DS_Store", "-type", "f", "-delete"], check=True)
 
     # Remove output_data if exists to start clean
     if os.path.isdir(output_data):
