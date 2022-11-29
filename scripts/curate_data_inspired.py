37038dc030dac96c2b0fd9f942441deb55f4bd91
valosekj <jan.valosek@upol.cz>
2022-11-29 14:07:55 -0500

    Implement MPM image BIDSification

--- scripts/curate_data_inspired.py
+++ scripts/curate_data_inspired.py
@@ -13,6 +13,7 @@
 # Authors: Jan Valosek
 
 import os
+import re
 import sys
 import shutil
 import json
@@ -74,7 +75,9 @@ def copy_file(path_file_in, path_dir_out, file_out):
         path_file_out = os.path.join(path_dir_out, file_out)
         print(f'Copying {path_file_in} to {path_file_out}')
         shutil.copyfile(path_file_in, path_file_out)
-        create_dummy_json_sidecar_if_does_not_exist(path_file_out)
+        # Create a dummy json sidecar for all files except of MPM (for MPM we copy original .json sidecars)
+        if 'MPM' not in file_out:
+            create_dummy_json_sidecar_if_does_not_exist(path_file_out)
 
 
 # TODO - do we want to create just a empty json sidecar? Or do we want to include some params there?
@@ -95,9 +98,52 @@ def read_json_file(path_to_file):
     with open(path_to_file) as p:
         loaded_json = json.load(p)
         series_description = loaded_json['acqpar'][0]['SeriesDescription']
-        flip_angle = loaded_json['acqpar'][0]['SeriesDescription']
+        flip_angle = loaded_json['acqpar'][0]['FlipAngle']
         echo_time = loaded_json['acqpar'][0]['EchoTime']
         return series_description, flip_angle, echo_time
+
+
+def construct_mpm_bids_filename(mpm_files_dict, path_output, subject_out):
+    """
+    Construct BIDS compliant filename for MPM images, e.g. 'acq-T1w_echo-1_flip-1_mt-off_MPM'
+    Then, call function to copy the input non-BIDS MPM file to BIDS compliant MPM file
+    :param mpm_files_dict: dict with SeriesDescription, FlipAngle, and EchoTime across all MPM files
+    :param path_output: path to output BIDS folder
+    :param subject_out: output BIDS subjectID containing centre name and pathology, e.g., 'sub-torontoDCM001'
+    :return:
+    """
+    mpm_to_bids = dict()
+    # Get unique values
+    unique_SeriesDescription = set([key[0] for key in mpm_files_dict.keys()])
+    unique_FlipAngle = set([key[1] for key in mpm_files_dict.keys()])
+    unique_EchoTime = set([key[2] for key in mpm_files_dict.keys()])
+    # Construct BIDS compliant filename, e.g. 'acq-T1w_echo-1_flip-1_mt-off_MPM'
+    for echo_idx, echo_time in enumerate(unique_EchoTime, start=1):
+        for flip_idx, flip_angle in enumerate(unique_FlipAngle, start=1):
+            for series_description in unique_SeriesDescription:
+                if '_mt_' in series_description:
+                    BIDS_label = 'acq-MTw' + '_echo-' + str(echo_idx) + '_flip-' + str(flip_idx) + '_mt-on_MPM'
+                elif '_pd_' in series_description:
+                    BIDS_label = 'acq-PDw' + '_echo-' + str(echo_idx) + '_flip-' + str(flip_idx) + '_mt-off_MPM'
+                elif '_t1_' in series_description:
+                    BIDS_label = 'acq-T1w' + '_echo-' + str(echo_idx) + '_flip-' + str(flip_idx) + '_mt-off_MPM'
+                # Find original MPM filename
+                if (series_description, flip_angle, echo_time) in mpm_files_dict.keys():
+                    mpm_file = mpm_files_dict[(series_description, flip_angle, echo_time)]
+                    mpm_to_bids[mpm_file] = BIDS_label
+
+    # Construct path to the output BIDS compliant directory
+    path_dir_out = os.path.join(path_output, subject_out, 'anat')
+    # Copy individual mpm raw files to the output BIDS directory
+    for sequence_params, path_file_in in mpm_files_dict.items():
+        label_out = mpm_to_bids[path_file_in]               # e.g., 'acq-MTw_echo-1_flip-1_mt-on_MPM'
+        # Construct output filename, e.g., 'sub-torontoDCM001_acq-MTw_echo-1_flip-1_mt-on_MPM.nii.gz'
+        file_out = subject_out + '_' + label_out + '.nii.gz'
+        # Copy MPM nii file
+        copy_file(path_file_in, path_dir_out, file_out)
+        # Copy MPM json sidecar
+        # Note: re.sub has to be used instead of .replace to match both '.nii' and '.nii.gz'
+        copy_file(re.sub('.nii(.gz)*', '.json', path_file_in), path_dir_out, file_out.replace('.nii.gz', '.json'))
 
 
 def get_parameters():
@@ -139,13 +185,13 @@ def main(path_input, path_output):
                     subject_in = subject_in.replace('.tar.gz', '')
                 # Loop across regions (brain or cord)
                 for region in ['brain', 'cord']:
+                    # Construct output subjectID containing centre name and pathology, e.g., 'sub-torontoDCM001'
+                    subject_out = prefix + centre_out + pathology_out + f"{sub_index:03d}"
                     if region == 'cord':
                         # Loop across files
                         for image_in, image_out in images_spine_conv_dict.items():
                             # Construct path to the input file
                             path_file_in = os.path.join(path_input, centre_in, pathology_in, subject_in, 'bl', region, image_in)
-                            # Construct output subjectID containing centre name and pathology, e.g., 'sub-torontoDCM001'
-                            subject_out = prefix + centre_out + pathology_out + f"{sub_index:03d}"
                             # Construct output filename, e.g., 'sub-torontoDCM001_bp-cspine_dir-AP_dwi.nii.gz'
                             file_out = subject_out + '_' + image_out
                             # Construct path to the output BIDS compliant directory
@@ -161,8 +207,6 @@ def main(path_input, path_output):
                         for image_in, image_out in images_brain_conv_dict.items():
                             # Construct path to the input file
                             path_file_in = os.path.join(path_input, centre_in, pathology_in, subject_in, 'bl', region, image_in)
-                            # Construct output subjectID containing centre name and pathology, e.g., 'sub-torontoDCM001'
-                            subject_out = prefix + centre_out + pathology_out + f"{sub_index:03d}"
                             # Construct output filename, e.g., 'sub-torontoDCM001_dir-AP_dwi.nii.gz'
                             file_out = subject_out + '_' + image_out
                             # Construct path to the output BIDS compliant directory
@@ -172,14 +216,21 @@ def main(path_input, path_output):
                             # Copy file and create a dummy json sidecar if does not exist
                             copy_file(path_file_in, path_dir_out, file_out)
 
-                        # Convert raw MPM images
+                        # Process raw MPM images
                         mpm_raw_folder_path = os.path.join(path_input, centre_in, pathology_in, subject_in, 'bl',
                                                            region, 'mpm_raw')
                         if os.path.isdir(mpm_raw_folder_path):
+                            mpm_files_dict = dict()
                             # Loop across individual MPM files
                             for mpm_file in glob.glob(os.path.join(mpm_raw_folder_path, '*.nii*')):
-                                series_description, flip_angle, echo_time = read_json_file(mpm_file.replace('.nii', '.json'))
-                                # TODO - finish MPM files conversion
+                                # Get SeriesDescription, FlipAngle, and EchoTime for each MPM file
+                                # Note: re.sub has to be used instead of .replace to match both '.nii' and '.nii.gz'
+                                series_description, flip_angle, echo_time = read_json_file(re.sub('.nii(.gz)*', '.json', mpm_file))
+                                # Collect SeriesDescription, FlipAngle, and EchoTime across all MPM files
+                                mpm_files_dict[series_description, flip_angle, echo_time] = mpm_file
+
+                            # Construct BIDS compliant filename for MPM images
+                            construct_mpm_bids_filename(mpm_files_dict, path_output, subject_out)
 
 
 if __name__ == "__main__":
